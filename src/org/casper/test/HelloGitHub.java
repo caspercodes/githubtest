8208f98672b2cfe076acdf0ba96187089ed7a3e0
Casper <Casper@maxingping>
2015-10-10 19:22:59 +0800

    第一次提交

---
+++ src/org/casper/test/HelloGitHub.java
@@ -0,0 +1,14 @@
+package org.casper.test;
+
+public class HelloGitHub {
+	/**
+	 * 仅仅作为测试github的使用
+	 * @Author Casper
+	 * Create_time:2015年10月10日 下午7:21:32
+	 * description:
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub!");
+	}
+
+}
